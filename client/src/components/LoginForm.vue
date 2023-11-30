35dfda16a0a008d35122ef47e328b88dcb603b30
MhdTarhini <mohamedtarhini95@gmail.com>
2023-11-30 23:50:54 +0200

    client <> Clean the Code

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -4,28 +4,23 @@
       <div class="card">
         <div class="card-body">
           <h5 class="card-title mb-4">Sign In</h5>
-          <form>
-              <p v-if="Object.keys(validationErrors).length != 0" class='text-center '><small class='text-danger'>Invalid
-                  Email or Password</small></p>
-              <div class="mb-3">
-                <label htmlFor="email" class="form-label">
-                  Email address
-                </label>
-                <input v-model="email" type="email" class="form-control" id="email" name="email" />
+          <form @submit.prevent="loginAction">
+            <p v-if="validationErrors.NotApproved" class="text-center">
+              <small class="text-danger">{{ validationErrors['NotApproved'] }}</small>
+            </p>
+            <div v-for="field in formFields" :key="field.name" class="mb-3">
+              <label :for="field.name" class="form-label">{{ field.label }}</label>
+              <input v-model="formData[field.name]" :type="field.type" :id="field.name" :name="field.name" class="form-control" />
+              <div v-if="validationErrors[field.name]" class="flex flex-col">
+                <small class="text-danger">{{ validationErrors[field.name][0] }}</small>
               </div>
-              <div class="mb-3">
-                <label htmlFor="password" class="form-label">Password</label>
-                <input v-model="password" type="password" class="form-control" id="password" name="password" />
-              </div>
-              <div class="d-grid gap-2">
-                <button :disabled="isSubmitting" @click="loginAction()" type="button"
-                  class="btn btn-primary btn-block">Login</button>
-                <p class="text-center">Don't have account?
-                  <router-link to="/register">Register here </router-link>
-                </p>
-              </div>
-            </form>
-          </div>
+            </div>
+            <div class="d-grid gap-2">
+              <button :disabled="isSubmitting" type="submit" class="btn btn-primary btn-block">Login</button>
+              <p class="text-center">Don't have an account? <router-link to="/register">Register here</router-link></p>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -38,48 +33,45 @@ export default {
   name: 'LoginForm',
   data() {
     return {
-      email: '',
-      password: '',
+      formData: {
+        email: '',
+        password: '',
+      },
       validationErrors: {},
       isSubmitting: false,
+      formFields: [
+        { name: 'email', label: 'Email address', type: 'email' },
+        { name: 'password', label: 'Password', type: 'password' },
+      ],
     };
   },
   created() {
-    if (localStorage.getItem('token') != "" && localStorage.getItem('token') != null) {
-      this.$router.push('/dashboard')
+    if (localStorage.getItem('token')) {
+      this.$router.push('/dashboard');
     }
   },
   methods: {
     async loginAction() {
-      this.isSubmitting = true
-      let userInfo = {
-        email: this.email,
-        password: this.password,
-      }
+      this.isSubmitting = true;
       const { loginAPI } = useAxios();
       try {
-        const response = await loginAPI(userInfo)
-        const userloginInfo = await response.data
-        if (userloginInfo.status == "success") {
-          localStorage.setItem('token', userloginInfo.authorisation.token);
-          if (userloginInfo.user.user_type_id === 1) {
-            // if admin
-            this.$router.push('/admin');
-          } else {
-            // if user
-            this.$router.push('/dashboard');
-          }
+        const response = await loginAPI(this.formData);
+        const userLoginInfo = await response.data;
+        if (userLoginInfo.status === 'success') {
+          localStorage.setItem('token', userLoginInfo.authorisation.token);
+          const userType = userLoginInfo.user.user_type_id;
+          this.$router.push(userType === 1 ? '/admin' : '/dashboard');
         } else {
-          this.isSubmitting = false
-          this.validationErrors = userloginInfo.status
+          this.isSubmitting = false;
+          this.validationErrors = { NotApproved: userLoginInfo.status };
         }
       } catch (error) {
-        this.isSubmitting = false
-        if (error.response.data.errors != undefined) {
-          this.validationErrors = error.response.data.errors
+        this.isSubmitting = false;
+        if (error.response.data.errors) {
+          this.validationErrors = error.response.data.errors;
         }
       }
-    }
+    },
   },
 };
 </script>
--- client/src/components/RegisterForm.vue
+++ client/src/components/RegisterForm.vue
@@ -1,124 +1,87 @@
 <template>
-    <div class="row justify-content-md-center mt-5">
-        <div class="col-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title mb-4">Register</h5>
-                    <form>
-                        <div class="mb-3">
-                            <label htmlFor="name" class="form-label">Name
-                            </label>
-                            <input type="text" class="form-control" id="name" name="name" v-model="name" />
-                            <div v-if="validationErrors.name" class="flex flex-col">
-                                <small class="text-danger">
-                                    {{ validationErrors?.name[0] }}
-                                </small>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label htmlFor="email" class="form-label">Email address
-                            </label>
-                            <input type="email" class="form-control" id="email" name="email" v-model="email" />
-                            <div v-if="validationErrors.email" class="flex flex-col">
-                                <small class="text-danger">
-                                    {{ validationErrors?.email[0] }}
-                                </small>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label htmlFor="password" class="form-label">Password
-                            </label>
-                            <input type="password" class="form-control" id="password" name="password" v-model="password" />
-                            <div v-if="validationErrors.password" class="flex flex-col">
-                                <small class="text-danger">
-                                    {{ validationErrors?.password[0] }}
-                                </small>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="bloodType" class="form-label">Blood Type</label>
-                            <select v-model="bloodType" class="form-select" id="bloodType" name="bloodType">
-                                <option value="">Select Blood Type</option>
-                                <option value="A+">A+</option>
-                                <option value="A-">A-</option>
-                                <option value="B+">B+</option>
-                                <option value="B+">O+</option>
-                                <option value="B+">O-</option>
-                            </select>
-                            <div v-if="validationErrors.password" class="flex flex-col">
-                                <small class="text-danger">
-                                    {{ validationErrors?.bloodType[0] }}
-                                </small>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="gender" class="form-label">Gender</label>
-                            <select v-model="gender" class="form-select" id="gender" name="gender">
-                                <option value="">Select Gender</option>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                            </select>
-                            <div v-if="validationErrors.password" class="flex flex-col">
-                                    <small class="text-danger">
-                                        {{ validationErrors?.gender[0] }}
-                                    </small>
-                            </div>
-                        </div>
-                        <div class="d-grid gap-2">
-                            <button :disabled="isSubmitting" @click="registerAction()" type="button"
-                                class="btn btn-primary btn-block">Register Now
-                            </button>
-                            <p class="text-center">Have already an account <router-link to="/">Login here</router-link></p>
-                        </div>
-                    </form>
-                </div>
+  <div class="row justify-content-md-center mt-5">
+    <div class="col-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title mb-4">Register</h5>
+          <form @submit.prevent="registerAction">
+            <div v-for="field in formFields" :key="field.name" class="mb-3">
+              <label :for="field.name" class="form-label">{{ field.label }}</label>
+              <select v-if="field.type === 'select'" v-model="formData[field.name]" :id="field.name" :name="field.name" class="form-select">
+                <option v-for="option in field.options" :key="option.value" :value="option.value">{{ option.label }}</option>
+              </select>
+              <input v-else-if="field.type === 'text' || field.type === 'password'" :type="field.type" v-model="formData[field.name]" :id="field.name" :name="field.name" class="form-control">
+              <div v-if="validationErrors[field.name]" class="flex flex-col">
+                <small class="text-danger">{{ validationErrors[field.name][0] }}</small>
+              </div>
             </div>
+            <div class="d-grid gap-2">
+              <button :disabled="isSubmitting" type="submit" class="btn btn-primary btn-block">Register Now</button>
+              <p class="text-center">Have already an account <router-link to="/">Login here</router-link></p>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
+  </div>
 </template>
-  
+
 <script>
 import { useAxios } from '../API/queries';
 
 export default {
-    name: 'RegisterForm',
-    data() {
-        return {
-            name: '',
-            email: '',
-            password: '',
-            bloodType:   '',
-            gender:   '',
-            validationErrors: {},
-            isSubmitting: false,
-        };
-    },
-    methods: {
-        async registerAction() {
-            this.isSubmitting = true
-            let userInfo = {
-                name: this.name,
-                email: this.email,
-                password: this.password,
-                bloodType: this.bloodType,
-                gender: this.gender,
-            }
-            const { registerAPI } = useAxios();
-            console.log(userInfo)
-
-            try {
-                const response = await registerAPI(userInfo)
-                const RegisterUser=await response.data
-                if(RegisterUser.status==="success"){
-                    this.$router.push('/login')
-                }
-            } catch (error) {
-                this.isSubmitting = false
-                if (error.response.data.errors != undefined) {
-                    this.validationErrors = error.response.data.errors
-                }
-            }
+  name: 'RegisterForm',
+  data() {
+    return {
+      formFields: [
+        { name: 'name', label: 'Name', type: 'text' },
+        { name: 'email', label: 'Email address', type: 'text' },
+        { name: 'password', label: 'Password', type: 'password' },
+        { name: 'bloodType', label: 'Blood Type', type: 'select', options: [
+            { label: 'Select Blood Type', value: '' },
+            { label: 'A+', value: 'A+' },
+            { label: 'A-', value: 'A-' },
+            { label: 'B+', value: 'B+' },
+            { label: 'O+', value: 'O+' },
+            { label: 'O-', value: 'O-' },
+          ]
+        },
+        { name: 'gender', label: 'Gender', type: 'select', options: [
+            { label: 'Select Gender', value: '' },
+            { label: 'Male', value: 'male' },
+            { label: 'Female', value: 'female' },
+          ]
+        },
+      ],
+      formData: {
+        name: '',
+        email: '',
+        password: '',
+        bloodType: '',
+        gender: '',
+      },
+      validationErrors: {},
+      isSubmitting: false,
+    };
+  },
+  methods: {
+    async registerAction() {
+      this.isSubmitting = true;
+      const { registerAPI } = useAxios();
+      
+      try {
+        const response = await registerAPI(this.formData);
+        const registerUser = await response.data;
+        if (registerUser.status === "success") {
+          this.$router.push('/login');
         }
-    },
+      } catch (error) {
+        this.isSubmitting = false;
+        if (error.response.data.errors !== undefined) {
+          this.validationErrors = error.response.data.errors;
+        }
+      }
+    }
+  },
 };
 </script>

823da7c555f8098d2990e73ab172cc2222e10831
MhdTarhini <mohamedtarhini95@gmail.com>
2023-11-30 17:30:16 +0200

    Client <> Init Bootstrap + Create Login Form

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -1,7 +1,53 @@
-<script setup>
-
-</script>
-
 <template>
-  <div>login Form</div>
+  <div class="row justify-content-md-center mt-5">
+    <div class="col-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title mb-4">Sign In</h5>
+          <form>
+            <div class="mb-3">
+              <label htmlFor="email" class="form-label">
+                Email address
+                </label>
+              <input v-model="email" type="email" class="form-control" id="email" name="email" />
+            </div>
+            <div class="mb-3">
+              <label htmlFor="password" class="form-label">Password
+              </label>
+              <input v-model="password" type="password" class="form-control" id="password" name="password" />
+            </div>
+            <div class="d-grid gap-2">
+              <button :disabled="isSubmitting" @click="loginAction()" type="button"
+                class="btn btn-primary btn-block">Login</button>
+               <p class="text-center">Don't have account?
+                <router-link to="/register">Register here </router-link>
+                </p>
+            </div>
+          </form>
+        </div>
+        </div>
+    </div>
+  </div>
 </template>
+  
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      email: '',
+      password: '',
+      isSubmitting: false,
+    };
+  },
+  methods: {
+    loginAction() {
+      this.isSubmitting = true
+      let UserInfo = {
+        email: this.email,
+        password: this.password,
+      }
+    }
+  },
+};
+</script>
---
+++ client/src/views/DashboardView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <main>
+        <div>Dashborad</div>
+    </main>
+</template>

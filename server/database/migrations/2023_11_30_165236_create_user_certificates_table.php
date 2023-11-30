1a7f1e5f9d9c991613692e263e4dc1ce50bea6ee
MhdTarhini <mohamedtarhini95@gmail.com>
2023-11-30 19:00:20 +0200

    server <> Migration of Database

--- server/database/migrations/2023_11_30_165236_create_user_certificates_table.php
+++ server/database/migrations/2023_11_30_165236_create_user_certificates_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_certificates', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('blood_type');
-            $table->string('gender');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('certificate_id');
+            $table->foreign('certificate_id')->references('id')->on('certificates');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_certificates');
     }
 };
--- server/database/migrations/2023_11_30_165354_create_user_certificate_details_table.php
+++ server/database/migrations/2023_11_30_165354_create_user_certificate_details_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('user_certificate_details', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->unsignedBigInteger('user_certificate_id');
+            $table->foreign('user_certificate_id')->references('id')->on('user_certificates');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->date('date_achieved');
+            $table->string('university');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('user_certificate_details');
     }
 };
